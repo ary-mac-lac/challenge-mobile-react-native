d33d49b74ec8d2af04ece72fc6106b964cd5d7d8
Aryella Lacerda <aryella.lacerda@gmail.com>
2020-11-07 10:51:32 -0300

    refactor: extend success fixture & add small fixture

--- App/Features/Listing/Fixtures/index.ts
+++ App/Features/Listing/Fixtures/index.ts
@@ -1,12 +1,17 @@
 import success from './success.json'
-import { buildResponse } from '../../../Utils'
+import small from './small.json'
 
+import { buildResponse } from '../../../Utils'
 import { CharacterDataWrapper } from '../../../Entities'
 
 export default {
   success: buildResponse<CharacterDataWrapper>({
     ok: true,
     data: success,
+  }),
+  small: buildResponse<CharacterDataWrapper>({
+    ok: true,
+    data: small,
   }),
   error: buildResponse<CharacterDataWrapper>({
     ok: false,
