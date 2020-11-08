0b17939b6befc586734268e04789fa62b6a61277
Aryella Lacerda <aryella.lacerda@gmail.com>
2020-11-08 10:30:54 -0300

    refactor: extract Animation & Navigation constants

---
+++ App/Constants/Animation.ts
@@ -0,0 +1,7 @@
+// If all screen animations are interpolated using the same breakpoints,
+// the animations will seem syncronized.
+
+export const BREAKPOINT_1 = 0
+export const BREAKPOINT_2 = 0.5
+export const BREAKPOINT_3 = 0.9
+export const BREAKPOINT_4 = 1
---
+++ App/Constants/Navigation.ts
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native'
+const { height } = Dimensions.get('screen')
+
+export const MODAL_HEIGHT = height * 0.8 // Picture this from bottom to top of screen
+export const MDOAL_GESTURE_RESPONSE_DISTANCE = height * 0.3 // Picture this from top to bottom of screen
+
+// Gesture response distance overlaps modal at the top so that users can touch the top of the modal to slide it down
